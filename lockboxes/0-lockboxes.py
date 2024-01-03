528575e76335af29e3bb6eda36c8fc68a2221255
grem62 <gremontmatheo22@gmail.com>
2024-01-03 09:39:50 +0100

    lockbox

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,24 +3,16 @@
 
 
 def canUnlockAll(boxes):
-    # Create a set to keep track of the opened boxes
-    opened_boxes = set([0])
-
-    # Create a list to keep track of the keys
-    keys = boxes[0]
-
-    # Iterate through the keys and boxes
-    while keys:
-        # Get the first key from the list
-        key = keys.pop(0)
-
-        # Check if the key opens a new box
-        if key < len(boxes) and key not in opened_boxes:
-            # Add the key to the opened boxes set
-            opened_boxes.add(key)
-
-            # Add the new keys to the list
-            keys.extend(boxes[key])
-
-    # Check if all boxes have been opened
-    return len(opened_boxes) == len(boxes)
+    """ Lockboxes """
+    if len(boxes) == 0:
+        return False
+    if len(boxes) == 1:
+        return True
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    if len(keys) == len(boxes):
+        return True
+    return False
