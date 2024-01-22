eb61d1cf814957144adfc1974d60830921db0c34
grem62 <gremontmatheo22@gmail.com>
2024-01-22 13:30:59 +0100

    correction env

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """_summary_
 """
 
 
 def minOperations(n):
     """_summary_
-
     Args:
-        n (_type_): _description_
+        n (int): _description_
+    Returns:
+        int: _description_
     """
-    div = 2
-    result = 0
-    while isinstance(n, int) and n > 1:
-        while n % div:
-            div += 1
-        result += div
-        n = n // div
-    return result
+    if n <= 1:
+        return 0
+    else:
+        i = 2
+        count = 0
+        while i <= n:
+            if n % i == 0:
+                count += i
+                n = n / i
+            else:
+                i += 1
+        return count
