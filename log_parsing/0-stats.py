e259fa34ad421ee5f7e3e276e0b0e52867381144
grem62 <gremontmatheo22@gmail.com>
2024-02-16 14:05:57 +0100

    stats

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+
+status_codes = {"200": 0, "301": 0, "400": 0,
+                "401": 0, "403": 0,
+                "404": 0, "405": 0, "500": 0}
+file_size = 0
+counter = 0
+
+try:
+    for line in sys.stdin:
+        counter += 1
+        data = line.split()
+        try:
+            file_size += int(data[-1])
+        except:
+            pass
+        try:
+            status_codes[data[-2]] += 1
+        except:
+            pass
+        if counter == 10:
+            print("File size: {}".format(file_size))
+            for key, value in status_codes.items():
+                if value != 0:
+                    print("{}: {}".format(key, value))
+            counter = 0
+except KeyboardInterrupt:
+    pass
