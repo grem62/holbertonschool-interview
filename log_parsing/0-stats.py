7d8725f27cc42a88f95390bdee159bc63e076d8b
grem62 <gremontmatheo22@gmail.com>
2024-02-16 14:47:04 +0100

    test3

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,35 +1,59 @@
 #!/usr/bin/python3
+"""0-stats.py"""
+
 import sys
 
 
-"""Script to get stats from a request"""
-codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-size = 0
-counter = 0
-status = {k: 0 for k in codes}
+"""script that reads stdin line by line and computes metrics:"""
+
+x = 0
+dict = {}
+status_available = [
+    '200',
+    '301',
+    '400',
+    '401',
+    '403',
+    '404',
+    '405',
+    '500']
+total_size = 0
+
+
+def print_stats(total_size, dict):
+    print("File size: {}".format(total_size))
+    for key in sorted(dict):
+        print("{}: {}".format(key, dict[key]))
+
+
 try:
     for line in sys.stdin:
-        counter += 1
-        data = line.split()
-        if len(data) > 2:
-            if data[-2] in status:
-                status[data[-2]] += 1
-            size += int(data[-1])
-        if counter == 10:
-            print("File size: {}".format(size))
-            for k, v in sorted(status.items()):
-                if v:
-                    print("{}: {}".format(k, v))
-            counter = 0
-            size = 0
-            status = {k: 0 for k in codes}
+        line_split = line.split()
+
+        try:
+            file_size = line_split[-1]
+            total_size += int(file_size)
+        except (IndexError, ValueError):
+            pass
+
+        try:
+            status_code = line_split[-2]
+            if status_code in status_available:
+                if status_code in dict:
+                    dict[status_code] += 1
+                else:
+                    dict[status_code] = 1
+        except (IndexError, ValueError):
+            pass
+
+        x += 1
+
+        if x == 10:
+            print_stats(total_size, dict)
+            x = 0
+
+    print_stats(total_size, dict)
+
 except KeyboardInterrupt:
-    print("File size: {}".format(size))
-    for k, v in sorted(status.items()):
-        if v:
-            print("{}: {}".format(k, v))
-    counter = 0
-    size = 0
-    status = {k: 0 for k in codes}
+    print_stats(total_size, dict)
     raise
-print("File size: {}".format(size))
