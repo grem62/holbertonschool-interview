b2e75e6c62e92c0ebe5589d7e8c1a0867655a32f
grem62 <gremontmatheo22@gmail.com>
2024-05-21 09:59:48 +0200

    correction betty

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -2,43 +2,46 @@
 #include <stdio.h> /* Assurez-vous d'inclure stdio.h pour printf */
 
 int advanced_binary(int *array, size_t size, int value)
+/*function for search number*/
 {
-    int mid;
-    int mid_value;
-    size_t i;
+	int mid;
+	int mid_value;
+	size_t i;
 
-    if (array == NULL || size == 0)
-        return -1;
+	if (array == NULL || size == 0)
+		return (-1);
 
-    mid = (size - 1) / 2;
-    mid_value = array[mid];
+	mid = (size - 1) / 2;
+	mid_value = array[mid];
 
-    printf("Searching in array: ");
-    for (i = 0; i < size; i++)
-    {
-        printf("%d", array[i]);
-        if (i != size - 1)
-            printf(", ");
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i != size - 1)
+			printf(", ");
+	}
+	printf("\n");
 
-    if (mid_value == value)
-    {
-        if (mid == 0 || array[mid - 1] != value)
-            return mid;
-        else
-            return advanced_binary(array, mid + 1, value);
-    }
-    else if (mid_value > value)
-    {
-        return advanced_binary(array, mid + 1, value);
-    }
-    else
-    {
-        int result = advanced_binary(array + mid + 1, size - mid - 1, value);
-        if (result == -1)
-            return -1;
-        else
-            return result + mid + 1;
-    }
+	if (mid_value == value)
+	{
+		if (mid == 0 || array[mid - 1] != value)
+			return (mid);
+		else
+			return (advanced_binary(array, mid + 1, value));
+	}
+	else if (mid_value > value)
+	{
+		return (advanced_binary(array, mid + 1, value));
+	}
+	else
+
+	{
+		int result = advanced_binary(array + mid + 1, size - mid - 1, value);
+
+		if (result == -1)
+			return (-1);
+		else
+			return (result + mid + 1);
+	}
 }
