7fb593b2ad09604d8fa3e70dff2cf26d7a882307
grem62 <gremontmatheo22@gmail.com>
2024-05-21 10:03:03 +0200

    correction betty

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,6 +1,18 @@
 #include "search_algos.h"
 #include <stdio.h> /* Assurez-vous d'inclure stdio.h pour printf */
 
+
+/*
+ * Fonction : advanced_binary
+ * Description : Cette fonction effectue une recherche binaire avancée dans un tableau trié à la recherche d'une valeur spécifique.
+ * Paramètres :
+ *   - array : Un pointeur vers le début du tableau d'entiers dans lequel la recherche doit être effectuée.
+ *   - size : La taille du tableau.
+ *   - value : La valeur entière à rechercher dans le tableau.
+ * Retour :
+ *   - L'indice de l'élément recherché dans le tableau si celui-ci est trouvé.
+ *   - -1 si l'élément n'est pas trouvé dans le tableau.
+ */
 int advanced_binary(int *array, size_t size, int value)
 /*function for search number*/
 {
